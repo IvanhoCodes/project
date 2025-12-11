0981981d076db7a2464185795117fb7b68b9f52a
Ivo Krijt <ivo.krijt@outlook.com>
2025-12-11 18:22:05 +0100

    Create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
+import { Home } from './home';
+import { Assigned } from './reports/assigned';
+import { Done } from './reports/done';
+import { Documentation } from './documentation';
+import { Settings } from './settings';
 
 export const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         component: Home,
     },
+    {
+        path: 'reports/assigned',
+        component: Assigned,
+    },
+    {
+        path: 'reports/done',
+        component: Done,
+    },
+    {
+        path: 'documentation',
+        component: Documentation,
+    },
+    {
+        path: 'settings',
+        component: Settings,
+    }
 ];
---
+++ src/app/documentation.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Layout } from './components/layout';
+
+@Component({
+  selector: 'page',
+  template: `
+    <layout>
+      Documentatie
+    </layout>
+  `,
+  imports: [ Layout ],
+  styles: `
+    .example-spacer {
+      flex: 1 1 auto;
+    }
+    layout {
+      background-color: #f5f5f5;
+    }
+  `,
+})
+export class Documentation {}
