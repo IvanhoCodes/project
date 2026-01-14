ffb3cb77e4d18a15efbfd1fe6f2ffb59c8b3b6ae
Ivo Krijt <ivo.krijt@outlook.com>
2026-01-14 12:55:11 +0100

    Move description to report instead of values

--- src/app/services/reports.service.ts
+++ src/app/services/reports.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { ReportType, DamageAssessment, OverdueMaintenance, InstallationInspection, Modifications } from "../models/reports.model";
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,38 @@ export class ReportsService {
 
   async getReports(): Promise<any> {
     return await firstValueFrom(this.http.get(this.apiUrl));
+  }
+
+  getDefaultReportValuesByType(type: ReportType): DamageAssessment | OverdueMaintenance | InstallationInspection | Modifications | undefined {
+    let values;
+    if (type === 'damage_assessment') {
+      values = {
+        damage_type: '',
+        new_damage: false,
+        urgent: false
+      };
+    } else if (type === 'overdue_maintenance') {
+      values = {
+        maintenance_type: '',
+        costs: "0-500",
+        urgent: false
+      };
+    } else if (type === 'installation_inspection') {
+      values = {
+        installation_type: '',
+        reported_fault: '',
+        test_procedure: '',
+        approved: false
+      };
+    } else if (type === 'modifications') {
+      values = {
+        documented_modifications: '',
+        carried_out_by: '',
+        description_modification: '',
+        action: '',
+      };
+    }
+
+    return values;
   }
 }
