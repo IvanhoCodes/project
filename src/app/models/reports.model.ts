a5db391652dca449569116ecc1220350c8039fe3
Ivo Krijt <ivo.krijt@outlook.com>
2026-01-14 12:54:14 +0100

    Move description to report instead of values

--- src/app/models/reports.model.ts
+++ src/app/models/reports.model.ts
@@ -4,7 +4,6 @@ export interface DamageAssessment {
     new_damage: boolean;
     damage_type: string;
     urgent: boolean;
-    description: string;
 }
 
 export interface OverdueMaintenance {
@@ -18,7 +17,6 @@ export interface InstallationInspection {
     reported_fault: string;
     test_procedure: string;
     approved: boolean;
-    description: string;
 }
 
 export interface Modifications {
@@ -26,13 +24,13 @@ export interface Modifications {
     carried_out_by: string;
     description_modification: string;
     action: string;
-    description: string;
 }
 
 export interface Report {
     name: string;
     date: Date;
     location: string;
+    description: string;
     type: string;
     assigned_to?: string;
     values: DamageAssessment | OverdueMaintenance | InstallationInspection | Modifications;
