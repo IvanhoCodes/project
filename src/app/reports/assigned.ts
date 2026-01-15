0fc783cb3329dad70c622637faa0d35666922829
Ivo Krijt <ivo.krijt@outlook.com>
2026-01-15 14:54:13 +0100

    Add functionality for adding reports

--- src/app/reports/assigned.ts
+++ src/app/reports/assigned.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { Layout } from '../components/layout';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { SelectedReport } from "../components/selectedReport";
 
 @Component({
   selector: 'page',
   template: `
     <layout>
       <h2>Toegewezen rapportages</h2>
+      <mat-card>
+        <mat-card-content>
+          <selected-report [report]="report" />
+        </mat-card-content>
+      </mat-card>
     </layout>
   `,
-  imports: [ Layout ],
+  imports: [Layout, MatCard, SelectedReport, MatCardContent],
   styles: `
     .example-spacer {
       flex: 1 1 auto;
@@ -18,4 +25,18 @@ import { Layout } from '../components/layout';
     }
   `,
 })
-export class Assigned {}
+export class Assigned {
+  report = {
+    id: 6, // Mock id
+    name: '',
+    date: new Date(),
+    location: '',
+    description: '',
+    type: 'damage_assessment',
+    values: {
+      damage_type: '',
+      new_damage: false,
+      urgent: false
+    }
+  }
+}
